1c32b1befed3d0311e67d407443bcf6de6263617
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-05-22 18:18:18 -0700

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,71 @@
- // Variables for the game to function. First variable creates an array that lists all of the options of the alphabet.
- let gameChoices = [
-        "a", "b", "c", "d", "e",
-        "f", "g", "h", "i", "j", 
-        "k","l", "m", "n", "o", 
-        "p", "q", "r", "s", "t", 
-        "u", "v", "w", "x","y", "z", ];
-let wins = 0;
-let losses = 0;
-let guessesLeft = 50;
-let letterPicked = [];
+//establlish array and variables
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var userChoice = [];
+var randomCharacterVar;
+var computerChoice;
+var wins = 0;
+var losses = 0; 
+var guessesLeft = 10;
+var guessesSoFarVar = [];
 
+// console.log(options);
 
- // This variable randomly chooses a choice from the options array. This is the Computer's guess.
-   let gameLetter = gameChoices[Math.floor(Math.random() * gameChoices.length)].toLowerCase();
+//call function for computer choice
+randomCharacter();
 
-   gameChoices.pop();
-   console.log(gameLetter)
-//Calculations
-   function reset() {
-       guessesLeft = 50;
-       letterPicked = []; 
-       gameLetter = gameChoices[Math.floor(Math.random() * gameChoices.length)].toLowerCase();
-       console.log(gameLetter) 
-   }
+// //user chooses one character a-z and we capture those choices
+document.onkeyup = function(event) {
+    var userChoice = String.fromCharCode(event.keyCode);
 
-    // This function is run whenever the user presses a key. 
-    document.onkeyup = function(event) {
-    // This variable determines which key was pressed.
-   let userGuess= event.key.toLowerCase();
+        // .toLowerCase();
+        userChoice = userChoice.toLowerCase();
 
-// Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses.
-   
-    document.getElementById("userGuesshtml").innerHTML = userGuess;
-    document.getElementById("winshtml").innerHTML = wins;
+        console.log(userChoice);
+
+    // //compare choices and determine a win or loss or tie - this is Lisa's code to emulate perhaps
+    if (userChoice === computerChoice) {
+        alert("Winner winner Chicken Dinner!");
+        wins = wins + 1;
+        document.getElementById("wins").innerHTML = wins;
+        randomCharacter();
+        guessesLeft = 10;
+        guessesSoFarVar = "Let's try this again!!";
+        document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
+        guessesSoFarVar = [];
+    } else if (guessesLeft > 0){
+        //CHRIS FEEDBACKpush and then loop to display
+        guessesSoFarVar.push(" " + userChoice);
+        document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
+        guessesLeft = guessesLeft - 1;
+        document.getElementById("guessesLeft").innerHTML = guessesLeft;
         
-    document.getElementById("losseshtml").innerHTML = losses;
+        // console.log(userChoice); 
+    }
+    else {
+        // increment losses print loses to screen 
+        losses = losses + 1;
+        document.getElementById("losses").innerHTML = losses;
 
-    document.getElementById("guessesLefthtml").innerHTML = guessesLeft;
-     
+        //restart game
+        randomCharacter();
+        //reset guesses left to 10
+        guessesLeft = 10;
+        guessesSoFarVar = "Let's try this again!!";
+        document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
+        guessesSoFarVar = [];
+        
+    }
+    // console.log(wins);    
+    }
+    
 
-function prepend(userGuess) {
 
-    document.getElementById("userGuesshtml").innerHTML = picked.join(", ");
-  
-  }
 
-  if (userGuess === gameLetter) {
-      wins++;
-      alert("Yes, the letter was" + gameLetter.toLowerCase() + "you're psychic!");
-      reset ();
-  } else {
-      guessesLeft--;
-      console.log(guessesLeft);
-      letterPicked.push(userGuess);
-      prepend(userGuess);
-  }
-
-  if (letterPicked < 1) {
-      losses++;
-      reset();
-      alert("You lose! Game over, try again.");
-  }
-
+// //computer chooses one random character a-z - used math random formula to create computer choice (computer to make a choice)
+function randomCharacter() {
+    var randomNumber = Math.floor(Math.random() * 
+        options.length);
+    computerChoice = options[randomNumber];
+    
+    console.log(computerChoice);
 }
-
-
-  
