bca9dbc6057b1c07bd4a3b157866c2c35ee7c1bf
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-05-25 22:09:16 -0700

    final touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,69 @@
-//establlish array and variables
+//Establish array and variables
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 var userChoice = [];
 var randomCharacterVar;
 var computerChoice;
 var wins = 0;
-var losses = 0; 
+var losses = 0;
 var guessesLeft = 10;
 var guessesSoFarVar = [];
 
-// console.log(options);
 
-//call function for computer choice
+//Call function for computerChoice to return a random character
 randomCharacter();
 
-// //user chooses one character a-z and we capture those choices
-document.onkeyup = function(event) {
+//User chooses one character a-z and we capture those choices
+document.onkeyup = function (event) {
     var userChoice = String.fromCharCode(event.keyCode);
 
-        // .toLowerCase();
-        userChoice = userChoice.toLowerCase();
+    // .toLowerCase() to convert the string to lowercase letters
+    userChoice = userChoice.toLowerCase();
 
-        console.log(userChoice);
+    console.log(userChoice);
 
-    // //compare choices and determine a win or loss or tie - this is Lisa's code to emulate perhaps
+    //Compare choices and determine a win or loss 
     if (userChoice === computerChoice) {
-        alert("Winner winner Chicken Dinner!");
+        alert("Winner Winner Chicken Dinner!");
         wins = wins + 1;
         document.getElementById("wins").innerHTML = wins;
         randomCharacter();
         guessesLeft = 10;
-        guessesSoFarVar = "Let's try this again!!";
+        guessesSoFarVar = "Let's try this again!";
         document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
         guessesSoFarVar = [];
-    } else if (guessesLeft > 0){
-        //CHRIS FEEDBACKpush and then loop to display
+    } else if (guessesLeft > 0) {
         guessesSoFarVar.push(" " + userChoice);
         document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
         guessesLeft = guessesLeft - 1;
         document.getElementById("guessesLeft").innerHTML = guessesLeft;
-        
+
         // console.log(userChoice); 
     }
     else {
-        // increment losses print loses to screen 
+        //Increment losses print losses to screen 
         losses = losses + 1;
         document.getElementById("losses").innerHTML = losses;
 
-        //restart game
+        //Restart game
         randomCharacter();
-        //reset guesses left to 10
+        //Reset guesses left to 10
         guessesLeft = 10;
-        guessesSoFarVar = "Let's try this again!!";
+        guessesSoFarVar = "Let's try this again!";
         document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
         guessesSoFarVar = [];
-        
+
     }
     // console.log(wins);    
-    }
-    
+}
 
 
 
-// //computer chooses one random character a-z - used math random formula to create computer choice (computer to make a choice)
+
+//Computer chooses one random character a-z - used math random formula to create computerChoice (computer to make a choice)
 function randomCharacter() {
-    var randomNumber = Math.floor(Math.random() * 
+    var randomNumber = Math.floor(Math.random() *
         options.length);
     computerChoice = options[randomNumber];
-    
+
     console.log(computerChoice);
 }
